abbc98453a8b53417252ce33421c00a80ffdcaea
Fredrik Hansen <fredrik.hansen@eve.sx>
2022-04-20 09:52:55 +0200

    Initial, install Nvidia CUDA drivers

---
+++ install_cuda.sh
@@ -0,0 +1,9 @@
+sudo wget -O /etc/apt/preferences.d/cuda-repository-pin-600 https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
+sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+
+sudo apt update
+sudo apt install cuda
+echo 'export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}' >> ~/.bashrc
+
+nvcc --version
