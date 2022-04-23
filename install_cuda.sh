0c533cc8fa5cfe99a9d481229aad02660dffbe3b
fredrik-hansen <98120861+fredrik-hansen@users.noreply.github.com>
2022-04-23 16:05:10 +0200

    Added nvidia gpu support to docker

--- install_cuda.sh
+++ install_cuda.sh
@@ -7,3 +7,13 @@ sudo apt install cuda
 echo 'export PATH=/usr/local/cuda/bin${PATH:+:${PATH}}' >> ~/.bashrc
 
 nvcc --version
+
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit nvidia-container-runtime
+sudo systemctl restart docker
+
+
+docker run --gpus all nvidia/cuda:10.0-base nvidia-smi
