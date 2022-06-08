1b1ef0ce9d2b9b7dffffad77cacab25391bc5a2c
fredrik-hansen <98120861+fredrik-hansen@users.noreply.github.com>
2022-06-08 14:29:36 +0200

    Update install_conda.sh

--- install_conda.sh
+++ install_conda.sh
@@ -1,3 +1,6 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
 ./Miniconda3-latest-Linux-x86_64.sh -b
+
+
+conda install -n base -c conda-forge jupyterlab_widgets
