600fffb58da564a4cb8d4409067a6e84a785b821
fredrik-hansen <98120861+fredrik-hansen@users.noreply.github.com>
2022-06-23 15:55:55 +0200

    Fixed bad GPG

--- install_cuda.sh
+++ install_cuda.sh
@@ -1,6 +1,10 @@
 sudo wget -O /etc/apt/preferences.d/cuda-repository-pin-600 https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/7fa2af80.pub
 sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/ /"
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb 
+sudo dpkg -i cuda-keyring_1.0-1_all.deb
+
+sudo apt-key del 7fa2af80
 
 sudo apt update
 sudo apt install cuda
