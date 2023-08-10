3ec56915a302542393d8c891f393510d445ceb78
Fredrik Hansen <fredrik.hansen@eve.sx>
2023-08-10 16:05:53 +0200

    FF unsnapped

--- post-install.sh
+++ post-install.sh
@@ -1,2 +1,13 @@
 sudo lvextend --extents +100%FREE /dev/mapper/ubuntu--vg-ubuntu--lv
 sudo resize2fs /dev/mapper/ubuntu--vg-ubuntu--lv
+
+
+
+#
+#Get rid of snap version of firefox
+#
+sudo snap remove firefox
+sudo add-apt-repository ppa:mozillateam/ppa
+echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
+sudo apt install firefox
+
