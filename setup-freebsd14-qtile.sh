c70fdfa8318ebc515c5fa4a1117ea1a8ec4315a1
fredrik-hansen <98120861+fredrik-hansen@users.noreply.github.com>
2024-07-12 11:44:53 +0200

    Create setup-freebsd14-qtile.sh

---
+++ setup-freebsd14-qtile.sh
@@ -0,0 +1,10 @@
+# TLDR Install qtile on FreeBSD 14
+pkg install x11-toolkits/pango
+pkg install audio/pulseaudio
+pkg install devel/glib20
+pkg install devel/gettext-runtime
+pkg install x11-wm/qtile
+pkg install py311-cairo-1.21.0,1 py311-cairocffi-1.4.0
+pkg install gdk-pixbuf2-2.42.10_3
+
+pkg install neovim
